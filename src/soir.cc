f862093b2b7b88b120031e28fa526d106a481a91
mxs <mxs@sbrk.org>
2019-03-05 23:25:50 +0000

    Add basic window handling.
    This CL:
    
    - creates a basic window from settings,
    - introduces the main loop.

--- src/soir.cc
+++ src/soir.cc
@@ -7,13 +7,56 @@ namespace soir {
 // Relative path to the core configuration file.
 constexpr const char *kCoreConfigPath = "etc/soir.yml";
 
+// Default width for the window -- core.width
+constexpr const int kDefaultWidth = 400;
+
+// Default height for the window -- core.height
+constexpr const int kDefaultHeight = 300;
+
+// Default title for the window -- core.title
+constexpr const char *kDefaultTitle = "Soir ~";
+
+// Default for fullscreen mode -- core.fullscreen
+constexpr bool kDefaultFullscreen = false;
+
 Status Soir::Init() {
   MOVE_OR_RETURN(core_config_, Config::LoadFromPath(kCoreConfigPath));
+  RETURN_IF_ERROR(InitWindow());
 
   return StatusCode::OK;
 }
 
-Status Soir::Run() { return StatusCode::OK; }
+Status Soir::InitWindow() {
+  const int width = core_config_->Get<int>("core.width", kDefaultWidth);
+  const int height = core_config_->Get<int>("core.height", kDefaultHeight);
+  const std::string title =
+      core_config_->Get<std::string>("core.title", kDefaultTitle);
+  const bool is_fullscreen =
+      core_config_->Get<bool>("core.fullscreen", kDefaultFullscreen);
+
+  unsigned int style = 0;
+  if (is_fullscreen) {
+    style |= sf::Style::Fullscreen;
+  }
+
+  window_ =
+      std::make_unique<sf::Window>(sf::VideoMode(width, height), title, style);
+
+  return StatusCode::OK;
+} // namespace soir
+
+Status Soir::Run() {
+  while (window_->isOpen()) {
+    sf::Event event;
+    while (window_->pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window_->close();
+      }
+    }
+  }
+
+  return StatusCode::OK;
+}
 
 } // namespace soir
 
--- src/soir.h
+++ src/soir.h
@@ -1,6 +1,8 @@
 #ifndef SOIR_H
 #define SOIR_H
 
+#include <SFML/Window.hpp>
+
 #include "config.h"
 #include "status.h"
 
@@ -8,11 +10,24 @@ namespace soir {
 
 class Soir {
 public:
+  // Initializes Soir:
+  //
+  // - loads configuration file,
+  // - sets up the window.
   Status Init();
+
+  // Main loop of Soir:
+  //
+  // - polls MIDI events,
+  // - refreshes the screen.
   Status Run();
 
 private:
+  // Initializes the main window.
+  Status InitWindow();
+
   std::unique_ptr<Config> core_config_;
+  std::unique_ptr<sf::Window> window_;
 };
 
 } // namespace soir
