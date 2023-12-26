d1b2743710f934173cc987e417e2d1b3652acff9
s. rannou <mxs@sbrk.org>
2023-12-26 11:13:19 +0100

    feat: add copilot/clang-format

---
+++ src/live.hh
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace live {
+
+constexpr const char* kVersion = "0.1.0-alpha.1";
+
+}  // namespace live
--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,5 @@
-int main(int ac, char **av) {
+#include "live.hh"
+
+int main(int ac, char** av) {
   return 0;
 }
