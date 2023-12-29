7b0198446be6ded36f47c514f648c25f74e6e60b
s. rannou <mxs@sbrk.org>
2023-12-29 13:52:46 +0100

    feat: introduce temporal recursion & beat

--- src/midi/engine.cc
+++ src/midi/engine.cc
@@ -13,7 +13,44 @@ Engine::~Engine() {}
 absl::Status Engine::Init(const Config& config) {
   LOG(INFO) << "Initializing engine";
 
+  SetBPM(config.Get<uint16_t>("midi.initial_bpm"));
+  auto status = Beat(absl::Now());
+  if (!status.ok()) {
+    LOG(ERROR) << "Unable to schedule first beat: " << status;
+    return status;
+  }
+
   running_ = true;
+
+  return absl::OkStatus();
+}
+
+void Engine::SetBPM(uint16_t bpm) {
+  LOG(INFO) << "Setting BPM to " << bpm;
+
+  bpm_ = bpm;
+  beat_us_ = 60.0 / bpm_ * 1000000;
+}
+
+absl::Status Engine::Beat(const absl::Time& now) {
+  LOG(INFO) << "Beat " << current_beat_;
+
+  current_beat_ += 1;
+
+  return Schedule(now + absl::Microseconds(beat_us_),
+                  [this](const absl::Time& now) { return Beat(now); });
+}
+
+absl::Status Engine::Schedule(const absl::Time& at, CallbackFunc func) {
+  LOG(INFO) << "Scheduling callback at " << at;
+
+  // This is stupid simple because we currently don't support
+  // scheduling callbacks from multiple threads. So it is assumed here
+  // we are running in the context of Run(). If we ever support
+  // external scheduling, we'll need to wake up the Run loop here in
+  // case the next scheduled callback changes.
+
+  callbacks_.insert({at, func});
 
   return absl::OkStatus();
 }
@@ -22,18 +59,45 @@ absl::Status Engine::Run() {
   py::scoped_interpreter guard;
 
   while (true) {
+    // We assume there is always at least one callback in the queue
+    // due to the beat scheduling.
+    auto next = callbacks_.begin();
+
     std::list<std::string> updates;
     {
       std::unique_lock<std::mutex> lock(loop_mutex_);
-      loop_cv_.wait(lock,
-                    [this] { return !code_updates_.empty() || !running_; });
+      loop_cv_.wait_until(lock, absl::ToChronoTime(next->at), [this, next] {
+        return !running_ || !code_updates_.empty() || next->at <= absl::Now();
+      });
+
       if (!running_) {
         LOG(INFO) << "Received stop signal";
         break;
       }
-      std::swap(updates, code_updates_);
+
+      if (!code_updates_.empty()) {
+        std::swap(updates, code_updates_);
+      }
     }
 
+    // Process next callback if time has passed.
+    if (next->at <= absl::Now()) {
+      callbacks_.erase(next);
+
+      // Here we don't use now() as a parameter to the callback to
+      // avoid drifts. Instead, we pass the time at which it is
+      // expected to be scheduled, so that any drift is corrected on
+      // the next scheduling.
+      auto status = next->func(next->at);
+      if (!status.ok()) {
+        LOG(ERROR) << "Callback failed: " << status;
+      }
+    }
+
+    // Code updates are performed in a second time, after the temporal
+    // recursions, to be as precise on time as possible. It's OK if a
+    // code update takes 10ms to be applied, but not OK if it's a kick
+    // event for example.
     for (const auto& code : updates) {
       try {
         py::exec(code.c_str(), py::globals(), py::globals());
@@ -71,11 +135,6 @@ absl::Status Engine::UpdateCode(const std::string& code) {
 
   LOG(INFO) << "Code update queued";
 
-  return absl::OkStatus();
-}
-
-absl::Status Engine::OnBeat() {
-  LOG(INFO) << "OnBeat";
   return absl::OkStatus();
 }
 
--- src/midi/engine.hh
+++ src/midi/engine.hh
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <absl/status/status.h>
+#include <absl/numeric/int128.h>
 #include <condition_variable>
 #include <mutex>
 
@@ -10,11 +10,32 @@ namespace py = pybind11;
 
 namespace maethstro {
 
+using CallbackFunc = std::function<absl::Status(const absl::Time& now)>;
+
 struct Callback {
-  std::chrono::high_resolution_clock::time_point time;
-  std::function<absl::Status()> func;
+  // When the callback is meant to be executed.
+  absl::Time at;
+
+  // Callbacks take the current time to be able to perform
+  // temporal recursion without drifts.
+  CallbackFunc func;
+
+  bool operator()(const Callback& a, const Callback& b) const {
+    return a.at < b.at;
+  }
 };
 
+// This is the main engine that runs the Python code and schedules
+// callbacks. It uses a temporal recursion pattern to avoid time
+// drifts (this is heavily inspired from Extempore & Sonic PI).
+//
+// Threading model:
+//
+// - all Python is executed from the running loop
+// - all callbacks are executed from the running loop
+// - code updates are pushed from an external thread via UpdateCode()
+//
+// This allows callbacks to interact with Python without GIL issues.
 class Engine {
  public:
   Engine();
@@ -26,20 +47,24 @@ class Engine {
   absl::Status Wait();
 
   // This is called from another thread to evaluate a piece of Python
-  // code coming from Matin.
+  // code coming from Matin. Code is executed from the Run() loop.
   absl::Status UpdateCode(const std::string& code);
+  absl::Status Schedule(const absl::Time& at, CallbackFunc func);
+  absl::Status Beat(const absl::Time& now);
 
-  absl::Status PushCallback(std::function<absl::Status()> func);
-
-  absl::Status OnBeat();
+  void SetBPM(uint16_t bpm);
 
  private:
-  std::list<Callback> callbacks_;
+  std::set<Callback, Callback> callbacks_;
 
   std::mutex loop_mutex_;
   std::condition_variable loop_cv_;
   std::list<std::string> code_updates_;
   bool running_;
+
+  uint64_t current_beat_;
+  uint64_t beat_us_;
+  uint16_t bpm_;
 };
 
 }  // namespace maethstro
