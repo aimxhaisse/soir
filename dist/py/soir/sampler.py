1e1e69e6a5478235c6fd9cdb26a000acd70e72c8
s. rannou <mxs@sbrk.org>
2024-12-26 23:37:44 +0100

    feat: sample pan to be in the -1.0, 1.0 range

--- dist/py/soir/sampler.py
+++ dist/py/soir/sampler.py
@@ -88,7 +88,7 @@ class Sampler:
             name: str,
             start: float = 0.0,
             end: float = 1.0,
-            pan: float = 0.5,
+            pan: float = 0.0,
     ):
         """Plays a sample by its given name. If there is no exact
         match, attempts to find one that contains the name (for
@@ -100,7 +100,7 @@ class Sampler:
             name: The name of the sample.
             start: When in the sample to start playing in the [0.0, 1.0] range.
             end: When in the sample to end playing in the [0.0, 1.0] range.
-            pan: The panning of the sample in the [0.0, 1.0] range.
+            pan: The panning of the sample in the [-1.0, 1.0] range.
         """
         assert_in_loop()
 
