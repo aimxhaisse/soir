0aef4e20556478cc8dd45ecf344e95011fd11f6b
s. rannou <mxs@sbrk.org>
2025-01-01 18:54:08 +0100

    feat: rework the install script to be simpler

--- install.sh
+++ install.sh
@@ -1,100 +1,147 @@
 #!/usr/bin/env bash
+# Soir installer.
+#
+# Some bits are inspired from the uv and the cast installer
+# scripts. Goal is to setup the dist directory in $SOIR_DIR (typically
+# under the $HOME/.soir directory and add the soir bin directory to
+# the PATH.
+#
+# It also looks for dependencies like `just` and `jq`, it doesn't try
+# to install them as it may be complex to handle all cases, goal is to
+# cover those in the documentation.
 
-set -eo pipefail
+SOIR_DIR=${SOIR_DIR:-$HOME/.soir}
 
-BASE_DIR="${XDG_CONFIG_HOME:-$HOME}"
-SOIR_DIR="${SOIR_DIR:-"$BASE_DIR/.soir"}"
-SOIR_BIN_DIR="$SOIR_DIR/bin"
+# Usage of the soir installer
+function usage() {
+    cat <<EOF
+Usage: $0 [options]
 
+Options:
+  -h, --help          Show this help
+EOF
+}
+
+# Check if a command exists
+function check_cmd() {
+    command -v "$1" > /dev/null 2>&1
+
+    return $?
+}
+
+# Print an error message to stderr and exit if command doesn't exist
+function need_cmd() {
+    if ! check_cmd "$1"
+    then
+        err "need '$1' (command not found)"
+    fi
+}
+
+# Print an error message to stderr and exit
+function err() {
+    local red
+    local reset
+
+    red=$(tput setaf 1 2>/dev/null || echo '')
+    reset=$(tput sgr0 2>/dev/null || echo '')
+
+    echo "${red}ERROR${reset}: $1" >&2
+
+    exit 1
+}
+
+# Run a command or print an error message and exit
+function ensure() {
+    if ! "$@"
+    then
+        err "command failed: $*"
+    fi
+}
+
+# Get the architecture of the system
 function get_arch {
+    need_cmd uname
+
     case $(uname -m) in
     arm64)
         echo "aarch64"
         ;;
     *)
-        echo "soir: architecture not supported."
+        err "architecture not supported."
         exit 1
         ;;
     esac
 }
 
+# Get the operating system of the system
 function get_os {
+    need_cmd uname
+
     case $(uname -s) in
     Darwin)
         echo "macos"
         ;;
     *)
-        echo "soir: operating system not supported."
+        err "operating system not supported."
         exit 1
         ;;
     esac
 }
 
-TAG=$(curl -s "https://api.github.com/repos/aimxhaisse/soir/releases/latest" | jq -r .tag_name)
-RELEASE="soir-${TAG}-$(get_arch)-$(get_os)"
+# Check dependencies
+function check_deps {
+    need_cmd python3.11
+    need_cmd poetry
+    need_cmd jq
+    need_cmd curl
+    need_cmd just
+    need_cmd ffmpeg
+}
 
-function install_deps {
-    if [[ "$OSTYPE" =~ ^darwin ]]; then
-        if ! command -v brew &> /dev/null; then
-            echo "soir: homebrew is required to install dependencies."
+# Download and install soir
+function download_and_install {
+    local os
+    local arch
+    local tag
+    local release
+
+    os=$(get_os)
+    arch=$(get_arch)
+    tag=$(curl -s "https://api.github.com/repos/aimxhaisse/soir/releases/latest" | jq -r .tag_name)
+    release="soir-${tag}-${arch}-${os}"
+
+    ensure mkdir -p "${SOIR_DIR}"
+    ensure curl -sL "https://github.com/aimxhaisse/soir/releases/download/${tag}/${release}.tar.gz" -o "${SOIR_DIR}/soir.tar.gz"
+    ensure tar -xf "${SOIR_DIR}/soir.tar.gz" -C "${SOIR_DIR}"
+    ensure rm -f "${SOIR_DIR}/soir.tar.gz"
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -s|--with-samples)
+            WITH_SAMPLES=1
+            ;;
+        *)
+            err "Unknown option: $1"
+            usage
             exit 1
-        fi
-        brew install --quiet just jq python@3.11 poetry curl ffmpeg
-    fi
-}
-
-function install_dist {
-    echo "Setting up distribution for ${TAG}"
-    mkdir -p ${SOIR_DIR}
-    cd "${SOIR_DIR}"
-    curl -Ls "https://github.com/aimxhaisse/soir/releases/download/${TAG}/${RELEASE}.tar.gz" -o "${SOIR_DIR}/soir-${TAG}.tar.gz"
-    tar -xzf "${SOIR_DIR}/soir-${TAG}.tar.gz" --strip-components=1 -C "${SOIR_DIR}"
-    rm -f "${SOIR_DIR}/soir-${TAG}.tar.gz"
-}
-
-function install_py {
-    echo "Setting up Python dependencies for ${TAG}"
-    poetry -C ${SOIR_DIR} env use -C ${SOIR_DIR} python3.11 
-    poetry -C ${SOIR_DIR} install -C ${SOIR_DIR}
-}
-
-function install_path {
-    case $SHELL in
-    */zsh)
-        PROFILE="${ZDOTDIR-"$HOME"}/.zshenv"
-        PREF_SHELL=zsh
-        ;;
-    */bash)
-        PROFILE=$HOME/.bashrc
-        PREF_SHELL=bash
-        ;;
-    */fish)
-        PROFILE=$HOME/.config/fish/config.fish
-        PREF_SHELL=fish
-        ;;
-    */ash)
-        PROFILE=$HOME/.profile
-        PREF_SHELL=ash
-        ;;
-    *)
-        echo "soir: could not detect shell, manually add ${SOIR_BIN_DIR} to your PATH."
-        exit 1
+            ;;
     esac
+    shift
+done
 
-    if [[ ":$PATH:" != *":${SOIR_BIN_DIR}:"* ]]; then
-        if [[ "$PREF_SHELL" == "fish" ]]; then
-            echo >> "$PROFILE" && echo "fish_add_path -a $SOIR_BIN_DIR" >> "$PROFILE"
-        else
-            echo >> "$PROFILE" && echo "export PATH=\"\$PATH:$SOIR_BIN_DIR\"" >> "$PROFILE"
-        fi
-    fi
-}
+check_deps && download_and_install
 
-echo "installing soir..."
-
-install_deps
-install_dist
-install_py
-install_path
-
-echo "soir was installed successfully to ${SOIR_BIN_DIR}, please restart your shell to apply changes."
+echo "> █▀ █▀█ █ █▀█"
+echo "> ▄█ █▄█ █ █▀▄ installed to ${SOIR_DIR}"
+echo ">"
+echo "> Add the following line to your shell configuration file:"
+echo ""
+echo "export SOIR_DIR=\"${SOIR_DIR}\""
+echo "export PATH=\"${SOIR_DIR}/bin:\$PATH\""
+echo ""
+echo "> Then restart your shell and run 'soir'."
