73f595fe31bdbb2e49463e38f2ab8017abe815c0
s. rannou <mxs@sbrk.org>
2025-01-19 09:31:37 +0100

    feat: refactor a bit to have the sample class in a dedicated file

--- src/core/dsp/sample_pack.hh
+++ src/core/dsp/sample_pack.hh
@@ -5,21 +5,11 @@
 #include <map>
 #include <mutex>
 
+#include "core/dsp/sample.hh"
 #include "utils/config.hh"
 
 namespace soir {
 namespace dsp {
-
-struct Sample {
-  std::string path_;
-  std::string name_;
-  std::vector<float> lb_;
-  std::vector<float> rb_;
-
-  float DurationMs() const;
-  float DurationMs(std::size_t samples) const;
-  std::size_t DurationSamples() const;
-};
 
 class SamplePack {
  public:
