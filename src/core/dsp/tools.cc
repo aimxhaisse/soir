2e6ef77631264d947aab2aeeee00c106be7cb03a
s. rannou <mxs@sbrk.org>
2025-01-28 09:20:13 +0100

    feat: add LFO facility

--- src/core/dsp/tools.cc
+++ src/core/dsp/tools.cc
@@ -1,5 +1,7 @@
 #include "tools.hh"
 
+#include <cmath>
+
 namespace soir {
 namespace dsp {
 
@@ -9,6 +11,24 @@ float LeftPan(float pan) {
 
 float RightPan(float pan) {
   return pan < 0.0f ? (1.0f + pan) : 1.0f;
+}
+
+float Bipolar(float unipolar) {
+  return (unipolar - 0.5f) * 2.0f;
+}
+
+float Unipolar(float bipolar) {
+  return (bipolar + 1.0f) / 2.0f;
+}
+
+float Fabs(float value) {
+  return value < 0.0f ? -value : value;
+}
+
+float FastSin(float x) {
+  // Investigate alternatives once we have real usecases where we need
+  // to go fast. No need to waste time on this for now.
+  return sin(x);
 }
 
 }  // namespace dsp
--- src/core/dsp/tools.hh
+++ src/core/dsp/tools.hh
@@ -3,8 +3,14 @@
 namespace soir {
 namespace dsp {
 
+static constexpr float kPi = 3.14159265358979323846f;
+
 float LeftPan(float pan);
 float RightPan(float pan);
+float Bipolar(float value);
+float Unipolar(float value);
+float Fabs(float value);
+float FastSin(float x);
 
 }  // namespace dsp
 }  // namespace soir
