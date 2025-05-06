98f3ae57e9bb4c71c147ae93289b8f24a687a1bd
s. rannou <mxs@sbrk.org>
2025-05-06 09:46:46 +0200

    feat: add the ability to run the website from the soir CLI

---
+++ src/core/rt/py/soir/cli/docs.py
@@ -0,0 +1,45 @@
+"""
+Documentation management module for Soir.
+
+This module provides utilities for building and serving Soir documentation.
+"""
+
+import sys
+import typer
+from mkdocs.__main__ import cli as mkdocs_cli
+
+from .utils import (
+    expand_env_vars,
+)
+
+
+app = typer.Typer(help="Documentation management commands", no_args_is_help=True)
+
+
+@app.command("serve")
+def serve_docs():
+    """
+    Serve the Soir documentation locally.
+    
+    This builds the documentation and starts a local server to view it.
+    """
+    mkdocs_cli([
+        'serve',
+        '--verbose',
+        '--config-file', expand_env_vars('$SOIR_DIR/www/mkdocs.yml'),
+        '--dev-addr', 'localhost:8000'
+    ])
+
+
+@app.command("build")
+def build_docs():
+    """
+    Build the Soir documentation.
+    
+    This builds the documentation without starting a server.
+    """
+    mkdocs_cli([
+        'build',
+        '--site-dir', 'site/',
+        '--config-file', 'www/mkdocs.yml'
+    ])
