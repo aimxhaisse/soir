4eda52beadf53a56bb3d4432883166c3d3e2ef2c
s. rannou <mxs@sbrk.org>
2025-05-10 22:20:34 +0200

    feat: add the ability to list sample packs

--- src/core/rt/py/cli.py
+++ src/core/rt/py/cli.py
@@ -10,6 +10,7 @@ from typing import Optional
 
 from soir.cli import session
 from soir.cli import docs
+from soir.cli import samples
 
 
 app = typer.Typer(
@@ -21,6 +22,7 @@ app = typer.Typer(
 
 app.add_typer(session.app, name="session", help="Manage Soir sessions")
 app.add_typer(docs.app, name="docs", help="Manage Soir documentation")
+app.add_typer(samples.app, name="samples", help="Manage sample packs")
 
 
 app()
