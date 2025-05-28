ca52308052b007535422f7ca1a8da1209a175c7c
s. rannou <mxs@sbrk.org>
2025-05-28 22:22:12 +0200

    feat: upgrade audio output to SDL3

--- src/core/audio_output.cc
+++ src/core/audio_output.cc
@@ -8,20 +8,25 @@ namespace soir {
 namespace {
 
 void ListAudioDevices() {
-  int num_devices = SDL_GetNumAudioDevices(0);
+  int num_devices = 0;
+  SDL_AudioDeviceID* devices = SDL_GetAudioPlaybackDevices(&num_devices);
   for (int i = 0; i < num_devices; i++) {
-    LOG(INFO) << "Audio device [" << i << "]: " << SDL_GetAudioDeviceName(i, 0);
+    LOG(INFO) << "Audio device [" << i
+              << "]: " << SDL_GetAudioDeviceName(devices[i]);
   }
+  SDL_free(devices);
 }
 
 }  // namespace
 
 absl::Status AudioOutput::GetAudioDevices(
     std::vector<std::pair<int, std::string>>* out) {
-  const int num = SDL_GetNumAudioDevices(0);
+  int num = 0;
+  SDL_AudioDeviceID* devices = SDL_GetAudioPlaybackDevices(&num);
   for (int i = 0; i < num; i++) {
-    out->push_back({i, SDL_GetAudioDeviceName(i, 0)});
+    out->push_back({i, SDL_GetAudioDeviceName(devices[i])});
   }
+  SDL_free(devices);
   return absl::OkStatus();
 }
 
@@ -31,28 +36,55 @@ AudioOutput::AudioOutput() {
   ListAudioDevices();
 }
 
-AudioOutput::~AudioOutput() {}
+AudioOutput::~AudioOutput() {
+  if (audio_stream_) {
+    SDL_DestroyAudioStream(audio_stream_);
+    audio_stream_ = nullptr;
+  }
+}
 
 absl::Status AudioOutput::Init(const utils::Config& config) {
   LOG(INFO) << "Initializing audio output";
 
+  int num_devices = 0;
+  SDL_AudioDeviceID* devices = SDL_GetAudioPlaybackDevices(&num_devices);
+
+  if (num_devices <= 0) {
+    SDL_free(devices);
+    LOG(ERROR) << "No audio devices available";
+    return absl::InternalError("No audio devices available");
+  }
+
+  auto device_index = config.Get<int>("soir.dsp.output.audio.device_id", 0);
+  if (device_index >= num_devices) {
+    SDL_free(devices);
+    LOG(ERROR) << "Invalid audio device index: " << device_index;
+    return absl::InternalError("Invalid audio device index");
+  }
+
+  SDL_AudioDeviceID device_id = devices[device_index];
+  auto device_name = SDL_GetAudioDeviceName(device_id);
+  SDL_free(devices);
+
   SDL_AudioSpec spec;
   SDL_zero(spec);
   spec.freq = kSampleRate;
-  spec.format = AUDIO_F32LSB;
+  spec.format = SDL_AUDIO_F32LE;
   spec.channels = kNumChannels;
-  spec.samples = config.Get<unsigned int>("soir.dsp.engine.block_size");
 
-  auto device_id = config.Get<int>("soir.dsp.output.audio.device_id", 0);
-  auto device_name = SDL_GetAudioDeviceName(device_id, 0);
+  LOG(INFO) << "Opening audio device: " << device_name << " (ID: " << device_id
+            << ")"
+            << " with sample rate: " << spec.freq
+            << ", format: " << SDL_GetAudioFormatName(spec.format)
+            << ", channels: " << static_cast<int>(spec.channels);
 
-  LOG(INFO) << "Opening audio device: " << device_name;
-  SDL_AudioSpec obtained;
-  device_id_ = SDL_OpenAudioDevice(device_name, 0, &spec, &obtained, 0);
-  if (device_id_ == 0) {
-    LOG(ERROR) << "Failed to open audio device: " << SDL_GetError();
-    return absl::InternalError("Failed to open audio device");
+  audio_stream_ = SDL_OpenAudioDeviceStream(device_id, &spec, nullptr, nullptr);
+  if (!audio_stream_) {
+    LOG(ERROR) << "Failed to open audio device stream: " << SDL_GetError();
+    return absl::InternalError("Failed to open audio device stream");
   }
+
+  device_id_ = SDL_GetAudioStreamDevice(audio_stream_);
 
   return absl::OkStatus();
 }
@@ -60,7 +92,10 @@ absl::Status AudioOutput::Init(const utils::Config& config) {
 absl::Status AudioOutput::Start() {
   LOG(INFO) << "Starting audio output";
 
-  SDL_PauseAudioDevice(device_id_, 0);
+  if (!SDL_ResumeAudioDevice(device_id_)) {
+    LOG(ERROR) << "Failed to resume audio device: " << SDL_GetError();
+    return absl::InternalError("Failed to resume audio device");
+  }
 
   return absl::OkStatus();
 }
@@ -68,7 +103,10 @@ absl::Status AudioOutput::Start() {
 absl::Status AudioOutput::Stop() {
   LOG(INFO) << "Stopping audio output";
 
-  SDL_PauseAudioDevice(device_id_, 1);
+  if (!SDL_PauseAudioDevice(device_id_)) {
+    LOG(ERROR) << "Failed to pause audio device: " << SDL_GetError();
+    return absl::InternalError("Failed to pause audio device");
+  }
 
   return absl::OkStatus();
 }
@@ -87,8 +125,11 @@ absl::Status AudioOutput::PushAudioBuffer(AudioBuffer& buffer) {
     }
   }
 
-  SDL_QueueAudio(device_id_, interleaved_buffer.data(),
-                 interleaved_buffer.size() * sizeof(float));
+  if (!SDL_PutAudioStreamData(audio_stream_, interleaved_buffer.data(),
+                              interleaved_buffer.size() * sizeof(float))) {
+    LOG(ERROR) << "Failed to queue audio data: " << SDL_GetError();
+    return absl::InternalError("Failed to queue audio data");
+  }
 
   return absl::OkStatus();
 }
